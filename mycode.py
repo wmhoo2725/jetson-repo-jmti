6c362a91427a4c8ba3892fb507f5e1a46753399b
wmhoo2725 <wmhoo27@gmail.com>
2026-01-19 12:38:14 +0800

    remove .info

---
+++ mycode.py
@@ -0,0 +1,9 @@
+# This is a simple Python introduction program
+
+name = input("What is your name? ")
+age = input("How old are you? ")
+
+print("Hello!", name)
+print("You are", age, "years old.")
+print("Nice to meet you!")
+
